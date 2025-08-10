68a29044b4ffba26df320614fcd7110ef2c95fb7
Matheus <matheusdaws66@gmail.com>
2025-08-10 17:19:40 -0300

    feat: adiciona autenticação com better-auth e atualiza esquema de banco de dados

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,7 +9,7 @@ const AuthPage = () => {
   return (
     <div className="h-screen w-screen items-center justify-center flex">
       <Tabs defaultValue="login">
-        <TabsList className="w-full justify-center">
+        <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="register">Criar conta</TabsTrigger>
         </TabsList>
---
+++ src/lib/auth.ts
@@ -0,0 +1,22 @@
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+  user: {
+    modelName: "usersTable",
+  },
+  session: {
+    modelName: "sessionTable",
+  },
+  account: {
+    modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
+  },
+});
